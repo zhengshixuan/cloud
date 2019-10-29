d5cf7b83a2a5907103d1fe07d0fde784fdf006c2
lianxq <zhengshixuan@qq.com>
2019-10-29 23:22:53 +0800

    无

---
+++ spring-cloud-hystrix-dashboard-9001/src/test/java/com/tianjian/SpringCloudHystrixDashboard9001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianjian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudHystrixDashboard9001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- spring-cloud-order-8081/src/main/java/com/tianjian/order/OrderController.java
+++ spring-cloud-order-8081/src/main/java/com/tianjian/order/OrderController.java
@@ -1,5 +1,6 @@
 package com.tianjian.order;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,19 @@ import java.util.List;
 @RestController
 public class OrderController {
     @RequestMapping("/list")
+    @HystrixCommand(fallbackMethod = "listOrders_hystrix")
     public List<String> listOrders() {
         ArrayList<String> strings = new ArrayList<>();
         strings.add("111");
+        if(strings.size()<2){
+            throw new RuntimeException("测试hystrix");
+        }
+        return strings;
+
+    }
+    public List<String> listOrders_hystrix() {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("listOrders_hystrix");
         return strings;
 
     }
